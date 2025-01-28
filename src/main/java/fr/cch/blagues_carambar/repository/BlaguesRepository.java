35f7f7e5851a69533e478d30c0a1bb711f9e52c5
Cedric Chimot <cedric_om10@yahoo.fr>
2025-01-28 09:24:59 +0100

    optimisation de codes

--- src/main/java/fr/cch/blagues_carambar/repository/BlaguesRepository.java
+++ src/main/java/fr/cch/blagues_carambar/repository/BlaguesRepository.java
@@ -2,17 +2,7 @@ package fr.cch.blagues_carambar.repository;
 
 import fr.cch.blagues_carambar.entity.Blagues;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface BlaguesRepository extends JpaRepository<Blagues, Long> {
-
-    /**
-     * Requête pour afficher une blague aléatoirement
-     * @return une blague aléatoire
-     */
-    @Query(value = "SELECT * FROM blagues ORDER BY RAND() LIMIT 1", nativeQuery = true)
-    Optional<Blagues> findRandomBlague();
 
 }
